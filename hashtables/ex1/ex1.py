f431b4a7c591d5bbdb62c136a68c61e8865b4755
Taylor <wwwaaavvveeerrr@gmail.com>
2019-08-09 11:47:00 -0400

    got half of the hash tables to pass

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,6 +8,21 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    index = 0
+    for i in weights:
+        index +=1
+        hash_table_insert(ht, i, index)
+
+    for i in range(len(weights)):
+        # checking hastable for the weights
+        sum_of_weight = limit - weights[i]
+        #this finds the answer
+        max_weight = hash_table_retrieve(ht, sum_of_weight)
+        if max_weight is not None:
+            return (max_weight, i)
+    # return None
+
+
 
     """
     YOUR CODE HERE
